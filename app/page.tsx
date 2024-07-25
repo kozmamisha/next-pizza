bf768be96a39fb37e7d17e69e52a83738015d680
Михайло Козьма <michailkozma05@gmail.com>
2024-07-25 16:03:36 +0300

    create top-bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+import { Categories, Container, SortPopup, Title, TopBar } from '@/components/shared';
 
 export default function Home() {
   return (
-    <div className="">
-      Main
-    </div>
+    <>
+      <Container className="mt-10">
+        <Title text="All pizzas" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
+    </>
   );
 }
